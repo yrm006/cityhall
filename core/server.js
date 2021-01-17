aa1ecba3ee091769b128227efc05918aeb9b9b3f
ken7python <ken.1215.banana@gmail.com>
2021-01-17 17:31:55 +0900

    server

--- core/server.js
+++ core/server.js
@@ -1,39 +1,118 @@
-//% deno run --allow-net --allow-read --allow-write server.js
-import { Application, Router ,send} from "https://deno.land/x/oak/mod.ts";
-import { DB } from "https://deno.land/x/sqlite/mod.ts";
-import { oakCors } from "https://deno.land/x/cors/mod.ts";
+var express = require("express");
+var app = express();
+
+var https = require('https');
+
+var fs = require('fs');
+
+var router = express.Router();
 
 
-const router = new Router();{
-    router.get("/", async function(ctx){
-        ctx.response.body = { message: "cityhall ver 0.1.0" };
+const cors = require('cors');
+
+const multer = require("multer");
+
+app.use(multer({dest: './public/uploads/'}).single('file'));
+
+app.use(cors());
+
+const bodyParser = require('body-parser');
+const { request } = require("http");
+const { response } = require("express");
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
+const sqlite = require('sqlite3').verbose();
+const db = new sqlite.Database('cityhall.db');
+
+var zh = false;
+
+app.post('/message', (req, res) => {
+    console.log(req.body);
+    var v = req.body;
+
+    db.serialize(() => {
+        const stmt = db.prepare("INSERT INTO TMessage (sBody,sUrl) VALUES (?,?)");
+        stmt.run([v.s,v.u]);
+        
+        stmt.finalize();
+      });
+    res.send({ message: "OK" });
+    zh = true;
+    console.log("情報が入りました。");
+    setTimeout(function(){
+        zh = false;
+    },1*60*1000);
+});
+
+app.get("/", (request, response) => {
+    response.status(200).send({ message: "OK" });
+});
+
+app.use(express.static('public'));
+app.post("/information",(req,res) => {
+    var v;
+
+    v = req.body;
+    console.log(v);
+    console.log(req.file);
+
+    db.serialize(() => {
+      const stmt = db.prepare("INSERT INTO information (title,text,image) VALUES (?,?,?)");
+      if (req.file != null){
+        stmt.run([v.title,v.text,req.file.filename]);
+      }
+      else{
+        stmt.run([v.title,v.text,null]);
+      }
+      stmt.finalize();
     });
-    router.post("/message", async function(ctx,res){
-        const v = await ctx.request.body().value;                                                    
-        console.log( v );
-        const db = new DB("cityhall.db");{
-            db.query("INSERT INTO TMessage (sBody,sUrl) VALUES (?,?)", [v.s,v.u]);
-            db.close();
-        }
-        ctx.response.body = { message: "OK" };
-    });
-    router.get("/message", async function(ctx){
-        let r = null;
+    res.send({message: "OK"});
+});
 
-        const db = new DB("cityhall.db");{
-            r = [...db.query("SELECT * FROM TMessage order by dtCreated desc").asObjects()];
-            db.close();
-        }
+app.get("/information", (req,res) => {
+    var r = [];
+    db.serialize(() => {
+        db.each("SELECT * FROM information", (error, row) => {
+          if(error) {
+            console.error('Error!', error);
+            return;
+          }
+          
+          r.push(row);
+        });
+      });
+      setTimeout(function(){
+        res.status(200).send(r);
+      },100);
+});
 
-        ctx.response.body = r;
-    });
-}
+app.get("/message", (request, response) => {
+    var r = [];
+    db.serialize(() => {
+        db.each("SELECT * FROM TMessage order by dtCreated desc", (error, row) => {
+          if(error) {
+            console.error('Error!', error);
+            return;
+          }
+          
+          r.push(row);
+        });
+      });
+      setTimeout(function(){
+        response.status(200).send(r);
+      },100);
+});
 
-const app = new Application();{
-    const port = 8080;
-    app.use(oakCors());
-    app.use(router.routes());
-    app.use(router.allowedMethods());
-                                                                        console.log('running on port ', port);
-    await app.listen({ port });
-}
+
+var ssl_server_key = 'server_key.pem';
+var ssl_server_crt = 'server_crt.pem';
+
+var options = {
+  key: fs.readFileSync(ssl_server_key),
+  cert: fs.readFileSync(ssl_server_crt)
+};
+
+https.createServer(options, app).listen(8080);
