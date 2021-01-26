4593e745f74a88a31676184cc9c03214a11d97dd
yrm <yrm@na-s.jp>
2021-01-26 10:28:54 +0900

    citizen: report

--- core/server.js
+++ core/server.js
@@ -1,21 +1,28 @@
 //% deno run --allow-net --allow-read --allow-write server.js
-import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+import { Application, Router} from "https://deno.land/x/oak/mod.ts";
 import { DB } from "https://deno.land/x/sqlite/mod.ts";
-
+import { oakCors } from "https://deno.land/x/cors/mod.ts";
 
 
 const router = new Router();{
     router.get("/", async function(ctx){
-        ctx.response.body = { message: "cityhall ver 0.1.0" };
+        ctx.response.body = { message: "cityhall ver 0.2.0" };
     });
-    router.post("/message", async function(ctx){
+    router.post("/report", async function(ctx,res){
         const v = await ctx.request.body().value;
-                                                                        console.log( v.s );
         const db = new DB("cityhall.db");{
-            db.query("INSERT INTO TMessage (sBody) VALUES (?)", [v.s]);
+            db.query("INSERT INTO TReport (sContent,sPhoto) VALUES (?,?)", [v.c,v.p]);
             db.close();
         }
-
+        ctx.response.body = { message: "OK" };
+    });
+    router.post("/message", async function(ctx,res){
+        const v = await ctx.request.body().value;
+        console.log( v );
+        const db = new DB("cityhall.db");{
+            db.query("INSERT INTO TMessage (sBody,sUrl) VALUES (?,?)", [v.s,v.u]);
+            db.close();
+        }
         ctx.response.body = { message: "OK" };
     });
     router.get("/message", async function(ctx){
@@ -32,11 +39,17 @@ const router = new Router();{
 
 const app = new Application();{
     const port = 8080;
-
+    
+    app.use(oakCors());
     app.use(router.routes());
     app.use(router.allowedMethods());
                                                                         console.log('running on port ', port);
-    await app.listen({ port });
+    await app.listen({
+        port: port,
+        secure: false,//!!!
+        certFile: "server_crt.pem",
+        keyFile: "server_key.pem",
+    });
 }
 
 
