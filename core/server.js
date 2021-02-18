2480283ec183337f69355b537c3cbcda70c4a4a9
ken7python <66853112+ken7python@users.noreply.github.com>
2021-02-18 18:02:58 +0900

    core

--- core/server.js
+++ core/server.js
@@ -26,6 +26,38 @@ const router = new Router();{
         }
         ctx.response.body = r;
     });
+
+    router.get("/report_chat", async function(ctx){
+        let r = null;
+        console.log( "/report_chat" );
+        const db = new DB("cityhall.db");{
+            r = [...db.query("SELECT * FROM TReportChat where pReport=? order by dtCreated desc",[6]).asObjects()];
+            db.close();
+        }
+        console.log(r);
+        ctx.response.body = r;
+    });
+
+    router.post("/report_chat_city", async function(ctx,res){
+        const v = await ctx.request.body().value;
+        console.log( v );
+        const db = new DB("cityhall.db");{
+            db.query("INSERT INTO TReportChat (sBody,pReport,bCity) VALUES (?,?,?)", [v.s,6,1]);
+            db.close();
+        }
+        ctx.response.body = { message: "OK" };
+    });
+    router.post("/report_chat_citizen", async function(ctx,res){
+        const v = await ctx.request.body().value;
+        console.log( v );
+        const db = new DB("cityhall.db");{
+            db.query("INSERT INTO TReportChat (sBody,pReport,bCity) VALUES (?,?,?)", [v.s,6,0]);
+            db.close();
+        }
+        ctx.response.body = { message: "OK" };
+    });
+
+
     router.post("/message", async function(ctx,res){
         const v = await ctx.request.body().value;
         console.log( v );
